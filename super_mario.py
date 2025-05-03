3bb7fb3b96e737a62e74cdbf131c6d09cf14a727
lokauni <hastighaffarpour@gmail.com>
2025-05-03 22:01:10 +0330

    Create a game window

--- super_mario.py
+++ super_mario.py
@@ -1,1 +1,19 @@
+import pygame
+import sys
 
+#Initialize pygame
+pygame.init()
+
+#Creat game window
+screen = pygame.display.set_mode((800,600))
+pygame.display.set_caption("Super Mario")
+
+#Main game loop
+while True:
+    for even in pygame.event.get():
+        if even.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    screen.fill((135, 206, 235))
+    pygame.display.update()
